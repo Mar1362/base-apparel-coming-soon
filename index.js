6c173453f70c7254275a78202c7a531c80c31e24
MAR <marmamadou1362@gmail.com>
2022-12-04 23:22:07 +0100

    finished the formular layout and behavior

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 const error = document.getElementById('error-messsage');
 const input = document.querySelector('input[type="email"]');
 
+input.onchange = function(){
+    if(input.checkValidity()) error.className = "hide";
+    else error.className = "show";
+}
